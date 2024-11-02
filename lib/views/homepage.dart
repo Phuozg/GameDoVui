ff4da463b939056c2dfe2edceacadc12c4475883
Nguyễn Tuấn Phương <122181908+Phuozg@users.noreply.github.com>
2024-11-03 01:17:48 +0700

    add-firebase

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,4 @@
-import 'package:dadd/games/gameScreen/game_Sceen.dart';
+import 'package:dadd/firebase_options.dart';
 import 'package:dadd/games/selectionScreen/chon_Screen.dart';
 import 'package:dadd/views/account_screen.dart';
 import 'package:dadd/views/expContainer.dart';
@@ -7,10 +7,14 @@ import 'package:dadd/views/historypage.dart';
 import 'package:dadd/views/menubutton.dart';
 import 'package:dadd/views/rankingpage.dart';
 import 'package:dadd/views/room_game_screen.dart';
-import 'package:dadd/views/solo_play.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -31,26 +35,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Trang Chủ'),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const AccountScreen()));
-          },
-          icon: const Icon(Icons.person),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              icon: const Icon(Icons.emoji_events),
-              onPressed: () {
-                Navigator.push(
-
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Trang Chủ'),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) => const AccountScreen()));
@@ -82,16 +73,16 @@ class HomePage extends StatelessWidget {
                 const EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
             child: Column(
               children: [
-
-               const Expcontainer(),
+                const Expcontainer(),
                 const SizedBox(height: 30),
-               
+
                 MenuButton(
                   text: 'Chơi Luyện Tập',
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const slc_Screen()),
+                      MaterialPageRoute(
+                          builder: (context) => const slc_Screen()),
                     );
                   },
                 ),
@@ -115,9 +106,9 @@ class HomePage extends StatelessWidget {
                   },
                 ),
                 MenuButton(
-                    text: 'Xem Lịch Sử Chơi',
-                    onPressed: () {
-                      showDialog(
+                  text: 'Xem Lịch Sử Chơi',
+                  onPressed: () {
+                    showDialog(
                         context: context,
                         builder: (BuildContext context) {
                           return Dialog(
@@ -125,10 +116,9 @@ class HomePage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20),
                             ),
                             child: CustomDialogContent(),
-
                           );
-                        })
-                  ],
+                        });
+                  },
                 ),
               ],
             ),
